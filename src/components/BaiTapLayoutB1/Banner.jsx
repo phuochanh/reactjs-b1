feb2c87b8e0c80757b38290181c3cf98e15a31d7
Phuoc Hanh <hanhcp2013@gmail.com>
2022-12-02 05:52:55 +0700

    done

--- src/components/BaiTapLayoutB1/Banner.jsx
+++ src/components/BaiTapLayoutB1/Banner.jsx
@@ -5,7 +5,7 @@ export default class Banner extends Component {
     return (
         <header className="py-5">
         <div className="container px-lg-5">
-          <div className="p-4 p-lg-5 bg-light rounded-3 text-center">
+          <div className="p-4 p-lg-5 bg-light rounded-3 text-left">
             <div className="m-4 m-lg-5">
               <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
               <p className="fs-4">Bootstrap utility classes are used to create this jumbotron since the old component has been removed from the framework. Why create custom CSS when you can use utilities?</p>
